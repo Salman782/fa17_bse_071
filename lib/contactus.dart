dcb1c6571163cf3478afd9bd618d3ca38a506ccf
Salman782 <shonamunda38@gmail.com>
2020-12-05 17:11:45 +0500

    contactus.dart

---
+++ lib/contactus.dart
@@ -0,0 +1,25 @@
+import 'package:fa17_bse_071/contactus.dart/';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: contactus(
+          email: ' @gmail.com',
+          companyName: ' ',
+          phoneNumber: ' 000',
+          githubUserName: ' salman',
+          instagramUserName: 'iamcancerian',
+        ),
+      ),
+    );
+  }
+}
