1e801f1c861571d33c87ad8aea9fb76d3aaf20f4
Salman782 <shonamunda38@gmail.com>
2020-12-05 16:09:02 +0500

    hardlevel

--- lib/HardLevel.dart
+++ lib/HardLevel.dart
@@ -9,7 +9,7 @@ class _HardLevelState extends State<HardLevel> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colorsorange[200],
+      backgroundColor: Colors.orange[200],
       appBar: AppBar(
         title: Text('Hard Level'),
       ),
